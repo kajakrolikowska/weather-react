f0605932e5d50bc7a8aa1f45a219029410fbbbac
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-12-09 20:31:17 +0100

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The temperature in ${props.city} is ${Math.round(
+        response.data.main.temp
+      )}°C`
+    );
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=8ade40df9cf169461fc7f8acab2e9ac0&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
